f9a9d36fa6927a53b197886c60bec08f36fc6fb6
henripal <henri.palacci@gmail.com>
2019-06-07 19:07:01 -0400

    refactor to module. first results

---
+++ fewshot/data.py
@@ -0,0 +1,105 @@
+import csv
+import os
+import PIL
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import torch
+import torchvision
+
+
+
+def fix_quotes(in_csv, out_csv):
+    """
+    fixes the problem with commas in last column of csv by quoting last column of csv
+    in_csv: string path to the raw csv file
+    out_csv: string path to the desired output file.
+        Will be created if not already existing. If already existing, file will be overwritten
+    """
+    with open(in_csv) as f, open(out_csv, 'w+') as o:
+        reader, writer = csv.reader(f), csv.writer(o)
+        for line in reader:
+            newline = line[:9] + [','.join(line[9:])]
+            writer.writerow(newline)
+
+
+def pil_loader(path):
+    with open(path, 'rb') as f:
+        img = PIL.Image.open(f)
+        return img.convert('RGB')
+
+def tensor2img(tensor):
+    """
+    converts tensor and displays image
+    """
+    rescaled = (tensor - tensor.min())/(tensor.max() - tensor.min())
+    plt.imshow(rescaled.numpy().swapaxes(0, 2).swapaxes(0, 1))
+
+class FashionData:
+    def __init__(self, csv_file, images_root, train_transform=None, test_transform=None, top20=False):
+        self.images_root = images_root
+        self.top20 = top20
+        self.csv_file = csv_file
+        
+        self.imagelist = [imagefile.split('.')[0] for imagefile in os.listdir(images_root) if imagefile.split('.')[0] != '']
+        self.label_data = pd.read_csv(csv_file)
+        self.filter_existing_images()
+        self.filter_top20(top20)
+        self.make_label_utils()
+        
+        
+        self.train_ds = FashionDataset(self.label_data[self.label_data.year % 2 == 0].reset_index(drop=True),
+                                      images_root,
+                                      train_transform)
+        self.test_ds = FashionDataset(self.label_data[self.label_data.year % 2 == 1].reset_index(drop=True),
+                                     images_root,
+                                     test_transform)
+        
+    def make_label_utils(self):
+        self.idx2name = self.label_data.articleType.value_counts().index.values
+        self.name2idx = {name: idx for idx, name in enumerate(self.idx2name)}
+            
+    def filter_existing_images(self):
+        """
+        truncates the dataframe to images that are found in path
+        """
+        self.label_data = self.label_data[self.label_data.id.isin(self.imagelist)]
+        if len(self.label_data) == 0: raise FileNotFoundError('Image directory is empty or does not match csv file')
+    
+    def filter_top20(self, top20):
+        sorted_labels = self.label_data.articleType.value_counts().index.values
+        if top20:
+            self.label_data = self.label_data[self.label_data.articleType.isin(sorted_labels[:20])]
+        else:
+            self.label_data = self.label_data[self.label_data.articleType.isin(sorted_labels[20:])]
+
+
+
+
+class FashionDataset(torch.utils.data.Dataset):
+    def __init__(self, label_data, images_root, transform=None):
+        """
+        csv_file: path to csv file containing the labels (styles.csv)
+        images_root: path to the images
+        transform: list of transforms we want to apply
+        train: whether we want to get the train images or the test images
+        top20: whether we want to limit the images to the top20 most frequent classes
+        """
+        self.images_root = images_root
+        self.transform = transform
+        self.label_data = label_data
+            
+    def __len__(self):
+        return len(self.label_data)
+    
+    def __getitem__(self, idx):
+        img_id, label = self.label_data.loc[idx, ['id', 'articleType']].values
+        filename = os.path.join(self.images_root, str(img_id) + '.jpg')
+        
+        img = pil_loader(filename)
+        
+        if self.transform:
+            img = self.transform(img)
+        
+        return img, label
---
+++ fewshot/trainer.py
@@ -0,0 +1,109 @@
+import torch
+import numpy as np
+import datetime
+import csv
+import os
+
+
+def run_training(model, n_epochs, data, optimizer, loss_func, gpu_id=0, root='./', bs=64):
+    """
+    basic training loop. not super customizable but has checkpointing and saves training traces
+    will create a folder and save models and training trace in that folder
+    """
+    train_dl = torch.utils.data.DataLoader(data.train_ds,
+                                 batch_size=bs, shuffle=True,
+                                 num_workers=4)
+    test_dl = torch.utils.data.DataLoader(data.test_ds,
+                                    batch_size=bs*2, shuffle=True,
+                                    num_workers=4)
+
+
+    torch.cuda.set_device(gpu_id)
+    model.cuda();
+    folder = root + 'gpu_' + str(gpu_id) + '_' + datetime.datetime.now().strftime("%b%d_%H%M") + '/'
+    if not os.path.exists(folder): os.mkdir(folder)
+
+    for epoch in range(n_epochs):
+        print('------------- epoch: ', epoch)
+        
+        # training
+        running_losses = []
+        model.train()
+        for images, labels in train_dl:
+            images = images.cuda()
+            labels = torch.tensor([data.name2idx[label] for label in labels]).cuda()
+            optimizer.zero_grad()
+            out = model(images)
+
+            loss = loss_func(out, labels)
+            running_losses.append(loss.item())
+            loss.backward()
+            optimizer.step()
+            
+        train_loss = '{:.3f}'.format(np.mean(running_losses))
+        print('Training loss:', train_loss)
+            
+        # validation
+        model.eval()
+        running_val_losses = []
+        running_accuracies = []
+        for images, labels in test_dl:
+            with torch.no_grad():
+                images = images.cuda()
+                labels = torch.tensor([data.name2idx[label] for label in labels]).cuda()
+                out = model(images)
+                loss = loss_func(out, labels)
+                running_val_losses.append(loss.item())
+                _, predicted = torch.max(out.data, 1)
+                running_accuracies.append((predicted == labels).float().mean().item())
+            
+        val_loss = '{:.3f}'.format(np.mean(running_val_losses))
+        print('Validation loss:', val_loss)
+        val_acc = '{:.3f}'.format(np.mean(running_accuracies))
+        print('Validation accuracy:', val_acc)
+
+        # checkpointing
+
+        with open(folder + 'trace.csv', 'a+', newline='') as f:
+            writer = csv.writer(f)
+            if epoch == 0: writer.writerow(['epoch', 'train_loss', 'val_loss', 'val_acc'])
+            writer.writerow([str(epoch), train_loss, val_loss, val_acc])
+
+        torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss,
+                    }, folder + 'model_epoch_' + str(epoch) + '.pth')
+
+def evaluate(model, data, gpu_id, n_classes, bs=64):
+    """
+    runs top-1 and top-5 accuracy on test set for the model for each class
+    """
+    test_dl = torch.utils.data.DataLoader(data.test_ds,
+                                    batch_size=bs*2, shuffle=True,
+                                    num_workers=4)
+    model.eval()
+
+    occurences = torch.zeros(n_classes).cuda()
+    correct1_all = torch.zeros(n_classes).cuda()
+    correct5_all = torch.zeros(n_classes).cuda()
+
+    for images, labels in test_dl:
+        with torch.no_grad():
+            images = images.cuda()
+            labels = torch.tensor([data.name2idx[label] for label in labels]).cuda()
+            out = model(images)
+            _, pred = out.topk(5, 1, True, True)
+            pred = pred.t()
+            correct = pred.eq(labels.view(1, -1).expand_as(pred))
+            correct1 = correct[:1].view(-1).float()
+            correct5 = correct[:5].float().sum(0)
+
+            correct1_all[labels] += correct1
+            correct5_all[labels] += correct5
+            occurences[labels] += 1
+
+    return correct1_all/occurences, correct5_all/occurences, occurences
+        
+
